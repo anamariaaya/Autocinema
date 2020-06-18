153ff0868ab286275e415f850bb25cf2a08d1aa8
anamariaaya <39868590+anamariaaya@users.noreply.github.com>
2020-06-17 19:54:18 -0500

    Integración de fecha proyección Index

--- Desarrollo/js/index.js
+++ Desarrollo/js/index.js
@@ -12,8 +12,11 @@ function getMovie(){
 	$.post('api-rest-autocinema/public/proyecciones',data).done(function(rest){
 		console.log(rest)
 		$("#peliculahoy").html('<h2>'+rest.proyecciones[0].titulo+'</h2><p>funciones '+rest.proyecciones[0].funciones[0]+'</p>')
+		$("#fechahoy").html('<p>'+rest.proyecciones[0].fechaProyeccion+'</p>')
 		$("#peliculamanana").html('<h2>'+rest.proyecciones[1].titulo+'</h2><p>funciones '+rest.proyecciones[1].funciones[0]+'</p>')
+		$("#fechamanana").html('<p>'+rest.proyecciones[1].fechaProyeccion+'</p>')
 		$("#peliculapasm").html('<h2>'+rest.proyecciones[2].titulo+'</h2><p>funciones '+rest.proyecciones[2].funciones[0]+'</p>')
+		$("#fechapasm").html('<p>'+rest.proyecciones[2].fechaProyeccion+'</p>')
 		
 	}).fail(function(err){
 			
